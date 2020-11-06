f221b6ab95330ce227ab6cbd268e123e6c149056
shozo <tateishi.shozo@gmail.com>
2020-11-06 07:04:46 +0000

    php11

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.profile')
+
+@section('title', 'Myプロフィールの作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィールの作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
