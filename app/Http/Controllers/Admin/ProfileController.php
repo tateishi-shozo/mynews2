7b526d1d3e9345affce869b75dd61f9bac066040
shozo <tateishi.shozo@gmail.com>
2020-11-09 10:59:36 +0000

    php16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -29,13 +29,27 @@ class ProfileController extends Controller
     return redirect('admin/profile/create');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+        abort(404); 
+      }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        
+        unset($profile_form['remove']);
+        unset($profile_form['_token']);
+        
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile/create');
     }
 }
--- database/migrations/2020_11_09_053110_create_profiles_table.php
+++ database/migrations/2020_11_09_053110_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfileTables extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProfileTables extends Migration
      */
     public function up()
     {
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('gender');
@@ -30,6 +30,6 @@ class CreateProfileTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profile');
+        Schema::dropIfExists('profiles');
     }
 }
